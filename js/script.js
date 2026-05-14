e72322083bff6dc00a924ce23738ceb5e989e4eb
Johanna Flock <Johanna.j.f@web.de>
2026-05-14 17:23:30 +0200

    fullscreen

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function closeModal(id) {
     document.getElementById(id).classList.add("d_none");
     document.body.classList.remove("no_scroll");
 }
+
+function toggleFullscreen() {
+    let game = document.getElementById("game_container");
+    if (!document.fullscreenElement) {
+        game.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
